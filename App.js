9d4ffa3c148ee29c6d65a18c85367ebe06769bd8
KikuchiYuta <kikkun0513@gmail.com>
2021-08-04 13:51:53 +0900

    メモ一覧ページのマークアップ

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello >World!!</Hello>
-      <StatusBar style="auto" />
+
+      {/* ヘッダー */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* メモリスト */}
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2021年8月4日 10:00</Text>
+          </View>
+          <View>
+            <Text>削除</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>宿題</Text>
+            <Text>2021年8月4日 12:00</Text>
+          </View>
+          <View>
+            <Text>削除</Text>
+          </View>
+        </View>
+
+      </View>
+
+      {/* 新規メモ作成ボタン */}
+      <View>
+        <Text>＋</Text>
+      </View>
+
     </View>
   );
 }
